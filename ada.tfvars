ad437bdbce311be0aa3cac87497550a028084f31
jltdsjrdev <jltdsjrdev@hotmail.com>
2024-11-12 13:57:42 -0300

    feat:adicionando arquivos

---
+++ ada.tfvars
@@ -0,0 +1,7 @@
+cidrvpc = "10.0.0.0/16"
+
+cidrpublica-a = "10.0.2.0/24"
+
+cidrpublica-b = "10.0.3.0/16"
+
+cidrpublica-c = "10.0.4.0/16"
---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "cidrvpc" {
+  description = "CIDR da VPC"
+  type = string  
+}
+
+variable "cidrpublica-a" {
+  description = "CIDR da publica a"
+  type = string  
+}
+
+variable "cidrpublica-b" {
+  description = "CIDR da publica b"
+  type = string
+}
+
+variable "cidrpublica-c" {
+  description = "CIDR da publica c"
+  type = string
+}
